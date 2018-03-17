4f9f4c77e11bbad731807ac706fd3423f6bd906e
Axel Clark <axelclark2@yahoo.com>
2018-03-16 20:33:48 -0400

    Initial project with Vue and Webpack

---
+++ assets/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Phoenix Vue</h1>
+    </header>
+    <main>
+      <hello></hello>
+    </main>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+</style>
